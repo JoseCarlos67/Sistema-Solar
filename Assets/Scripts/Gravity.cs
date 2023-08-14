80de93e20c53ea584f14acb51099de1bc512bd14
JoseCarlos67 <carlos6767jose@gmail.com>
2023-08-14 16:31:34 -0300

    Configurado os planetas, faltando o Sol e a Lua. (Sem script)

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    readonly float G = 100f;
+    GameObject[] celestials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        celestials = GameObject.FindGameObjectsWithTag("Celestial");
+        InitialVelocity();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        UGravity();
+    }
+
+    void UGravity()
+    {
+        foreach(GameObject a in celestials)
+        {
+            foreach(GameObject b in celestials)
+            {
+                if (!a.Equals(b))
+                {
+                    float m1 = a.GetComponent<Rigidbody>().mass;
+                    float m2 = b.GetComponent<Rigidbody>().mass;
+                    float r = Vector3.Distance(a.transform.position, b.transform.position);
+
+                    a.GetComponent<Rigidbody>().AddForce(
+                        (b.transform.position - a.transform.position).normalized *
+                        (G * (m1 * m2) / (r * r)));
+                }
+            }
+        }
+    }
+
+    void InitialVelocity()
+    {
+        foreach(GameObject a in celestials)
+        {
+            foreach(GameObject b in celestials)
+            {
+                if (!a.Equals(b)){
+                    float m2 = b.GetComponent<Rigidbody>().mass;
+                    float r = Vector3.Distance(a.transform.position, b.transform.position);
+                    a.transform.LookAt(b.transform);
+
+                    a.GetComponent<Rigidbody>().velocity += a.transform.right * Mathf.Sqrt((G * m2) / r);
+                }
+            }
+        }
+    }
+
+}
