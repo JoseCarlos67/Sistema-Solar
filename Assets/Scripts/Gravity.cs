939c576d2144f420d4b2415243e86fbbaf6dd091
JoseCarlos67 <carlos6767jose@gmail.com>
2023-08-15 08:31:41 -0300

    Remodelado o script de órbita dos planetas

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -2,64 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gravity : MonoBehaviour
+public class Orbit : MonoBehaviour
 {
-    readonly float G = 100f;
-    GameObject[] celestials;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        celestials = GameObject.FindGameObjectsWithTag("Celestial");
-        InitialVelocity();
-    }
+    public Transform centerObject; // O objeto em torno do qual o planeta orbita
+    public float orbitSpeed = 1.0f; // Velocidade de órbita em unidades por segundo
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Calcula a posição do planeta na órbita
+        Vector3 offset = transform.position - centerObject.position;
+        Quaternion rotation = Quaternion.Euler(0, orbitSpeed * Time.deltaTime, 0);
+        offset = rotation * offset;
+        transform.position = centerObject.position + offset;
+
+        // Mantém a rotação do planeta olhando para o centro do sistema (opcional)
+        transform.LookAt(centerObject);
     }
-
-    private void FixedUpdate()
-    {
-        UGravity();
-    }
-
-    void UGravity()
-    {
-        foreach(GameObject a in celestials)
-        {
-            foreach(GameObject b in celestials)
-            {
-                if (!a.Equals(b))
-                {
-                    float m1 = a.GetComponent<Rigidbody>().mass;
-                    float m2 = b.GetComponent<Rigidbody>().mass;
-                    float r = Vector3.Distance(a.transform.position, b.transform.position);
-
-                    a.GetComponent<Rigidbody>().AddForce(
-                        (b.transform.position - a.transform.position).normalized *
-                        (G * (m1 * m2) / (r * r)));
-                }
-            }
-        }
-    }
-
-    void InitialVelocity()
-    {
-        foreach(GameObject a in celestials)
-        {
-            foreach(GameObject b in celestials)
-            {
-                if (!a.Equals(b)){
-                    float m2 = b.GetComponent<Rigidbody>().mass;
-                    float r = Vector3.Distance(a.transform.position, b.transform.position);
-                    a.transform.LookAt(b.transform);
-
-                    a.GetComponent<Rigidbody>().velocity += a.transform.right * Mathf.Sqrt((G * m2) / r);
-                }
-            }
-        }
-    }
-
 }
